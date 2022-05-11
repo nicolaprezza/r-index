cbeb40f293a72a9fbae866ddf7b0b238c2d5f6a4
Nicola Prezza <nicolapr@gmail.com>
2022-05-11 09:55:57 +0200

    Update sparse_hyb_vector.hpp

--- internal/sparse_hyb_vector.hpp
+++ internal/sparse_hyb_vector.hpp
@@ -1,4 +1,8 @@
- /*
+// Copyright (c) 2017, Nicola Prezza.  All rights reserved.
+// Use of this source code is governed
+// by a MIT license that can be found in the LICENSE file.
+
+/*
   * sparse_hyb_vector: a wrapper on hyb_vector<> of the sdsl library, with support for rank/select1
   */
 
